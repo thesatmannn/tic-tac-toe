13e53fc833224e78af67fc785d19ca082ffc79ff
thesatmannn <seanom12@gmail.com>
2023-04-06 12:33:55 -0500

    add array display

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,24 +1,18 @@
 const gameBoard = (() => {
-     const myGameBoard = [];
-     const turn = 0;
+  const myGameBoard = ['x', 'x', 'o', 'x', 'x', 'x', 'o', 'x', 'x'];
+  const turn = 0;
 
-     for(i = 0; i < 9; i++) {
-        myGameBoard.push("");
-     }
- 
-     const squares = document.querySelector(".gameboard");
+  const squares = document.querySelector('.gameboard');
 
-     myGameBoard.forEach((item, index) => {
-        const square = document.createElement("div");
-        square.className = "square";
-        squares.appendChild(square);
-     })
-   
+  const player = (name, symbol) => ({ name, symbol });
 
+  const player1 = player('Player1', 'X');
+  const player2 = player('Player2', 'O');
+
+  myGameBoard.forEach((square, index) => {
+    square = document.createElement('div');
+    square.className = 'square';
+    square.innerHTML = myGameBoard[index];
+    squares.appendChild(square);
+  });
 })();
-
-const player = (name, symbol) => ({name, symbol});
-
-const player1 = player("Player1", "X");
-const player2 = player("Player2", "O");
-
