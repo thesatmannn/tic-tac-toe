9d6a6b7264fff4b59de96d9d73c3544570e13e85
thesatmannn <seanom12@gmail.com>
2023-04-27 12:20:46 -0500

    add logic that properly displays the winner and reset button that clears board and starts game over

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,64 @@
 const gameBoard = (() => {
-  const myGameBoard = ['', '', '', '', '', '', '', '', ''];
+  let myGameBoard = ['', '', '', '', '', '', '', '', ''];
   const squares = document.querySelector('.gameboard');
   const startBtn = document.getElementsByClassName('start')[0];
   const player1Input = document.getElementById('player1');
   const player2Input = document.getElementById('player2');
   const gameflow = document.querySelector('.gameflow');
   gameflow.innerText = 'Enter names and hit start';
+  const resetBtn = document.createElement('button');
+  resetBtn.innerText = 'Play again?';
+  resetBtn.classList.add('reset');
+  squares.appendChild(resetBtn);
   let turn = 0;
   let player1;
   let player2;
+  let gameOver = false;
+
+  const winConditions = [
+    // horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // diagonal
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const checkWinner = (symbol) => {
+    for (let i = 0; i < winConditions.length; i++) {
+      const [a, b, c] = winConditions[i];
+      if (myGameBoard[a] === symbol && myGameBoard[b] === symbol && myGameBoard[c] === symbol) {
+        gameOver = true;
+        if (symbol === 'X') {
+          gameflow.innerText = `${player1.name} wins!`;
+          resetBtn.style.visibility = 'visible';
+        } else {
+          gameflow.innerText = `${player2.name} wins!`;
+          resetBtn.style.visibility = 'visible';
+          resetBtn.innerText = 'Play again?';
+        }
+      }
+    }
+    if (!gameOver && turn === 9) {
+      gameflow.innerText = 'It\'s a tie!';
+      resetBtn.style.visibility = 'visible';
+    }
+  };
+
+  resetBtn.addEventListener('click', () => {
+    myGameBoard = ['', '', '', '', '', '', '', '', ''];
+    squares.childNodes.forEach((square) => (square.innerText = ''));
+    turn = 0;
+    gameOver = false;
+    resetBtn.style.visibility = 'hidden';
+    gameflow.innerText = "Let's play again!";
+    resetBtn.innerText = 'Play again?';
+  });
 
   startBtn.addEventListener('click', () => {
     if (player1Input.value == '' || player2Input.value == '') {
@@ -18,31 +68,38 @@ const gameBoard = (() => {
       player1Input.style.visibility = 'hidden';
       player2Input.style.visibility = 'hidden';
       squares.style.visibility = 'visible';
-      player1 = player(player1Input.value, 'X'); // Update to get input value for player1
-      player2 = player(player2Input.value, 'O'); // Update to get input value for player2
-      gameflow.innerText = `${player1.name}'s turn!`;
+      player1 = player(player1Input.value, 'X');
+      player2 = player(player2Input.value, 'O');
+      gameflow.innerText = 'Time to play!';
       document.getElementById('player1label').style.display = 'none';
       document.getElementById('player2label').style.display = 'none';
-      gameflow.style.marginBotton = '0px';
     }
   });
 
-  myGameBoard.forEach((square) => {
+  myGameBoard.forEach((square, index) => {
     square = document.createElement('div');
     square.className = 'square';
     squares.appendChild(square);
+    square.setAttribute('data-index', index);
     square.addEventListener('click', () => {
-      if (turn === 0 || turn % 2 === 0 && square.innerText === '') {
-        square.innerText = player1.symbol;
-        turn++;
-        gameflow.innerText = `${player2.name}'s turn!`;
-      } else if (turn !== 0 && turn % 2 !== 0 && square.innerText === '') {
-        square.innerText = player2.symbol;
-        turn++;
-        gameflow.innerText = `${player1.name}'s turn!`;
+      if (gameOver === false) {
+        if (turn === 0 || turn % 2 === 0 && square.innerText === '') {
+          square.innerText = player1.symbol;
+          myGameBoard[index] = player1.symbol;
+          turn++;
+          checkWinner(player1.symbol);
+        } else if (turn !== 0 && turn % 2 !== 0 && square.innerText === '') {
+          square.innerText = player2.symbol;
+          myGameBoard[index] = player2.symbol;
+          turn++;
+          checkWinner(player2.symbol);
+        }
+        if (!gameOver && turn === 9) {
+          gameflow.innerText = 'It\'s a tie!';
+          resetBtn.style.visibility = 'visible';
+        }
       }
     });
   });
 })();
-
 const player = (name, symbol) => ({ name, symbol });
